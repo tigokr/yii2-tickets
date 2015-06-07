17e74de3b5335bd06c512d0569dd689de841bc4d
tigo <tigokr@gmail.com>
2015-06-07 20:39:19 +0300

    menu improv

--- Module.php
+++ Module.php
@@ -2,6 +2,8 @@
 
 namespace tigokr\tickets;
 
+use yii\helpers\ArrayHelper;
+
 class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
 {
     public $controllerNamespace = 'tigokr\tickets\controllers';
@@ -11,11 +13,17 @@ class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
     public function bootstrap($app)
     {
         if ($app instanceof \yii\web\Application) {
-            $app->getUrlManager()->addRules([
-                $this->id => $this->id . '/' . $this->defaultRoute,
-                $this->id . '/<controller>' => $this->id . '/<controller>/index',
+/*            $app->getUrlManager()->addRules([
                 $this->id . '/<controller>/<action>' => $this->id . '/<controller>/<action>',
-            ], false);
+                $this->id . '/<controller>' => $this->id . '/<controller>/index',
+                $this->id => $this->id . '/' . $this->defaultRoute,
+            ], false);*/
+
+            if(isset($this->params['topMenu']))
+                $this->addTopMenu($this->params['topMenu']);
+
+            if(isset($this->params['mainMenu']))
+                $this->addMineMenu($this->params['mainMenu']);
         }
     }
 
@@ -32,5 +40,15 @@ class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
             return new $this->author;
         else
             return null;
+    }
+
+    private function addTopMenu($topMenu)
+    {
+        $this->module->params['topMenu'] = ArrayHelper::merge($topMenu, $this->module->params['topMenu']);
+    }
+
+    private function addMineMenu($mainMenu)
+    {
+        $this->module->params['mainMenu'] = ArrayHelper::merge($this->module->params['mainMenu'], $mainMenu);
     }
 }
--- config.php
+++ config.php
@@ -17,5 +17,18 @@ return [
             'vysg81@gmail.com',
         ],
         'noreplyEmail' => 'noreply@c21center.ru',
+
+        'topMenu' => [
+            'tickets' => ['label' => '<i class="fa fa-envelope-o"></i>', 'url' => '#', 'options' => ['class' => 'dropdown tasks-menu'], 'linkOptions' => ['aria-expanded' => 'false', 'class' => 'dropdown-toggle', 'data-toggle' => 'dropdown'], 'encode' => false, 'items' => [
+                ['label' => false, 'url' => null, 'items' => [
+                    ['label' => '&nbsp;<i class="fa fa-lightbulb-o text-success"></i> &nbsp;Внести предложение', 'url' => ['/tickets/default/message', 'type' => 30]],
+                    ['label' => '&nbsp;<i class="fa fa-exclamation text-warning"></i> &nbsp;Пожаловаться', 'url' => ['/tickets/default/message', 'type' => 20]],
+                    ['label' => '<i class="fa fa-exclamation-triangle text-danger"></i> Сообщить об ошибке', 'url' => ['/tickets/default/message', 'type' => 10]],
+                ], 'itemsOptions' => ['class' => 'menu'],],
+            ], 'itemsOptions' => ['class' => 'dropdown-menu']],
+        ],
+        'mainMenu' => [
+            ['label' => '<i class="fa fa-envelope"></i> Сообщения', 'url' => ['/tickets/default/index'], 'visible' => \Yii::$app->user->can('employee')],
+        ]
     ],
 ];
